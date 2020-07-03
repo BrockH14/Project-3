2b178d19fcfc656dbdc0dca90a0b10089fb0ce01
hunterhilado <hunterhilado@gmail.com>
2020-07-03 19:45:48 -0400

    HEro

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron";
+import Hero from "./components/Hero";
 import Cards from "./components/Cards";
 import Profile from "./pages/Profile";
 import Home from './pages/Home';
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        <Jumbotron />
+        <Hero />
         <Route exact path="/" component={Home}/>
         <Route exact path="/profile" component={Profile} />
         <Cards />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,41 @@
 import React, { Component } from "react";
 import Form from "../components/Form";
 import { render } from "@testing-library/react";
+import Hero from "../components/Hero";
 
 class Home extends Component {
-    state = {
-        saved: [],
-        q: "",
-    }
+  state = {
+    saved: [],
+    q: "",
+  };
 
-    handleInputChange = event => {
-        const {name, value} = event.target;
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
 
-        this.setState({
-            [name]: value
-        });
-    }
+    this.setState({
+      [name]: value,
+    });
+  };
 
-    handleFormSubmit = event => {
-        event.preventDefault();
-        
-        
-    }
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
 
-    render() {
+  render() {
     return (
-        <div>
-            <Form
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit}
-                q={this.state.q}
-            />
-        </div>
-    )
-    }
+      <div>
+        <Hero backgroundImage="https://www.monsterinsights.com/wp-content/uploads/2019/11/breathtaking-online-shopping-statistics-you-never-knew-625x300.png">
+          <h1>Shopalooza</h1>
+          <h2>Compare tons of different products!</h2>
+        </Hero>
+        <Form
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+          q={this.state.q}
+        />
+      </div>
+    );
+  }
 }
 
 export default Home;
